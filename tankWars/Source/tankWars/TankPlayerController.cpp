abb379e974a020f28fd28e27e413adbc6dec7254
Krystian_Wojakiewicz <Krysh97@gmail.com>
2018-07-31 13:43:06 +0200

    Line Trace from Player Camera to terrain
    De - projecting crosshairs into 3D space and using it to line trace

--- tankWars/Source/tankWars/TankPlayerController.cpp
+++ tankWars/Source/tankWars/TankPlayerController.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
+#include "Runtime/Engine/Classes/GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+#include "Runtime/Engine/Classes/Camera/PlayerCameraManager.h"
 
 void ATankPlayerController::BeginPlay() {
 	
@@ -18,8 +21,82 @@ void ATankPlayerController::BeginPlay() {
 
 }
 
+
+void ATankPlayerController::Tick(float DeltaTime) {
+
+	Super::Tick(DeltaTime);
+	AimCrosshairs();
+	
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	
 	return Cast<ATank>(GetPawn());
 }
+
+
+void ATankPlayerController::AimCrosshairs() {
+	
+	// if there's no tank - return
+	if (!GetControlledTank()) { return; }
+	FVector OutHitLocation;
+
+	if (GetAimLocation(OutHitLocation)) {
+		//Rotate tank's turret to AimLocation
+	}
+}
+
+
+bool ATankPlayerController::GetAimLocation(FVector& OutHitLocation) const {
+	
+	// Get 2D pixel coordinates for crosshairs location
+	int32 ViewPort_X, ViewPort_Y ;
+
+	GetViewportSize(ViewPort_X, ViewPort_Y);
+	FVector2D CrosshairLocation = FVector2D(ViewPort_X * CrossHair_X, ViewPort_Y * CrossHair_Y);
+	
+	
+	// "de-project" the 2D coord into 3D space and LineTrace the projection
+	FVector LookDirection;
+	FHitResult HitResult;
+
+	if (GetTraceHitLocation(CrosshairLocation, LookDirection, HitResult)) {
+		
+		OutHitLocation = HitResult.Location;
+		UE_LOG(LogTemp, Warning, TEXT("TraceEnd Coordinates %s"), *OutHitLocation.ToString())
+		return true;
+	}
+	
+
+	return false;
+}
+
+bool ATankPlayerController::GetTraceHitLocation(FVector2D CrosshairLocation, FVector& LookDirection, FHitResult& HitResult) const {
+
+	FVector CameraLocation;
+
+	// de-projecting
+	DeprojectScreenPositionToWorld(
+			CrosshairLocation.X,
+			CrosshairLocation.Y,
+			CameraLocation, // OUT parameter to get player camera location
+			LookDirection // Unit vector representing direction of crosshairs
+
+		);
+
+	auto response = FCollisionResponseParams();
+	auto params = FCollisionQueryParams();
+
+	//Line Trace
+	return GetWorld()->LineTraceSingleByChannel(
+			HitResult, // Saves the result in this OUT parameter
+			CameraLocation, // StartPoint
+			CameraLocation + LookDirection * LineTraceRange, // EndPoint
+			ECollisionChannel::ECC_Visibility,
+			params,
+			response
+
+		);
+}
+
 
--- tankWars/Source/tankWars/TankPlayerController.h
+++ tankWars/Source/tankWars/TankPlayerController.h
@@ -16,13 +16,32 @@ class TANKWARS_API ATankPlayerController : public APlayerController
 	
 public:
 
-	ATank* GetControlledTank() const;
+	virtual void Tick(float DeltaTime) override;
 
 	virtual void BeginPlay() override;
 
 private:
+	
+	ATank * GetControlledTank() const;
+	// Moves tank's turret to crosshairs location
+	void AimCrosshairs();
 
+	// Gets the coordinates of Object hit by Line Trace. Returns 1 if hit terrain or enemy tank, 0 otherwise
+	bool GetAimLocation(FVector& OutHitLocation) const;
+
+
+
+	// de-projects 2D vector of crosshair to 3D world vector and Line Traces
+	bool GetTraceHitLocation(FVector2D CrosshairLocation, FVector & LookDirection, FHitResult& HitResult) const;
 	
 
+
+	UPROPERTY(EditAnywhere)
+	float CrossHair_X = 0.5;
 	
+	UPROPERTY(EditAnywhere)
+	float CrossHair_Y = 0.333333;
+	
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 10000000.f;
 };
